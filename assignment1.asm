ebd85bc062cfe6f8f964c6a3217718121a67e7b5
Ramneet-Singh <ramneet2001@gmail.com>
2021-02-23 14:40:36 +0530

    Fixed Output for Equal X Coordinates

--- assignment1.asm
+++ assignment1.asm
@@ -278,16 +278,10 @@ y_read_complete:
     
     sub $s0, $s0, 1 # n <= n-1
 
-    # Move x2,y2 to x1,y1 if not(x1==x2 AND y1>y2)
-    
-    bne $s1, $s3, notEqualX
-    bgt $s2, $s4, skipMove
-    
-    notEqualX:
+    # Move x2,y2 to x1,y1 
     move $s1, $s3
     move $s2, $s4
     
-    skipMove:
     bgtz $s0, computeLoop 
     j terminate
 
